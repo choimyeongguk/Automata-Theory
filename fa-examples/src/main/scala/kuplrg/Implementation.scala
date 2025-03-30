b3e3c41b5a106cb7e8527033c35e536ec1aa1210
Choi Myeongguk <choimk0512@gmail.com>
2025-03-30 15:47:53 +0900

    complete implement

--- fa-examples/src/main/scala/kuplrg/Implementation.scala
+++ fa-examples/src/main/scala/kuplrg/Implementation.scala
@@ -43,12 +43,13 @@ object Implementation extends Template {
 
   // (Problem #1) DFA for L = { a b^n | n >= 1 }
   def dfa_a_b_plus: DFA = DFA(
-    states = Set(0, 1, 2),
+    states = Set(0, 1, 2, 3),
     symbols = Set('a', 'b'),
     trans = Map(
-      (0, 'a') -> 2, (0, 'b') -> 1,
-      (1, 'a') -> 1, (1, 'b') -> 1, 
-      (2, 'a') -> 1, (2, 'b') -> 2,
+      (0, 'a') -> 1, (0, 'b') -> 3,
+      (1, 'a') -> 3, (1, 'b') -> 2, 
+      (2, 'a') -> 3, (2, 'b') -> 2,
+      (3, 'a') -> 3, (3, 'b') -> 3,
     ),
     initState = 0,
     finalStates = Set(2),
@@ -74,8 +75,8 @@ object Implementation extends Template {
     symbols = Set('0', '1'),
     trans = Map(
       (0, '0') -> 1, (0, '1') -> 0,
-      (1, '0') -> 0, (1, '1') -> 2,
-      (2, '0') -> 0, (2, '1') -> 3,
+      (1, '0') -> 1, (1, '1') -> 2,
+      (2, '0') -> 2, (2, '1') -> 3,
       (3, '0') -> 3, (3, '1') -> 3,
     ),
     initState = 0,
@@ -85,18 +86,13 @@ object Implementation extends Template {
   // (Problem #4) DFA for L = { w \in {0, 1}* | w contains even number of 0's
   // and even number of 1's }
   def dfa_even_0_1: DFA = DFA(
-    states = Set(0, 1, 2, 3, 4, 5, 6, 7, 8),
+    states = Set(0, 1, 2, 3),
     symbols = Set('0', '1'),
     trans = Map(
-      (0, '0') -> 3, (0, '1') -> 1,
-      (1, '0') -> 4, (1, '1') -> 2,
-      (2, '0') -> 5, (2, '1') -> 0,
-      (3, '0') -> 6, (3, '1') -> 4,
-      (4, '0') -> 7, (4, '1') -> 5,
-      (5, '0') -> 8, (5, '1') -> 3,
-      (6, '0') -> 0, (6, '1') -> 7,
-      (7, '0') -> 1, (7, '1') -> 8,
-      (8, '0') -> 2, (8, '1') -> 6,
+      (0, '0') -> 2, (0, '1') -> 1,
+      (1, '0') -> 3, (1, '1') -> 0,
+      (2, '0') -> 0, (2, '1') -> 3,
+      (3, '0') -> 1, (3, '1') -> 2,
     ),
     initState = 0,
     finalStates = Set(0),
@@ -183,10 +179,31 @@ object Implementation extends Template {
   )
 
   // (Problem #8) ENFA for L = { (aba)^n | n >= 1 }
-  def enfa_aba_plus: ENFA = ???
+  def enfa_aba_plus: ENFA = ENFA(
+    states = Set(0, 1, 2, 3),
+    symbols = Set('a', 'b'),
+    trans = Map(
+      (0, Some('a')) -> Set(1),
+      (1, Some('b')) -> Set(2),
+      (2, Some('a')) -> Set(3),
+      (3, None)      -> Set(0),
+    ),
+    initState = 0,
+    finalStates = Set(3),
+  )
 
   // (Problem #9) ENFA for L = { 0^n | n >= 1 } U { 1^n | n >= 1 }
-  def enfa_same_digits: ENFA = ???
+  def enfa_same_digits: ENFA = ENFA(
+    states = Set(0, 1, 2),
+    symbols = Set('0', '1'),
+    trans = Map(
+      (0, Some('0')) -> Set(1), (0, Some('1')) -> Set(2),
+      (1, Some('0')) -> Set(1),
+      (2, Some('1')) -> Set(2),
+    ),
+    initState = 0,
+    finalStates = Set(1, 2),
+  )
 
   // (Problem #10) ENFA for L = L_1 \cap L_2
   // where
@@ -194,5 +211,21 @@ object Implementation extends Template {
   // L_2 = { w \in {0, 1} | zeros(w) \equiv 1 (mod 3) }
   // N(w) is the natural number represented by w in binary,
   // and zeros(w) is the number of 0's in w
-  def enfa_complex: ENFA = ???
+  def enfa_complex: ENFA = ENFA(
+    states = Set(0, 1, 2, 3, 4, 5, 6, 7, 8),
+    symbols = Set('0', '1'),
+    trans = Map(
+      (0, Some('0')) -> Set(1), (0, Some('1')) -> Set(3),
+      (1, Some('0')) -> Set(2), (1, Some('1')) -> Set(4),
+      (2, Some('0')) -> Set(0), (2, Some('1')) -> Set(5),
+      (3, Some('0')) -> Set(7), (3, Some('1')) -> Set(0),
+      (4, Some('0')) -> Set(8), (4, Some('1')) -> Set(1),
+      (5, Some('0')) -> Set(6), (5, Some('1')) -> Set(2),
+      (6, Some('0')) -> Set(4), (6, Some('1')) -> Set(6),
+      (7, Some('0')) -> Set(5), (7, Some('1')) -> Set(7),
+      (8, Some('0')) -> Set(3), (8, Some('1')) -> Set(8),
+    ),
+    initState = 0,
+    finalStates = Set(4),
+  )
 }
